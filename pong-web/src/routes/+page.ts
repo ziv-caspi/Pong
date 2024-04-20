0947f634670d30082698cee920e7a926b8079fc9
ziv-caspi <caspifx@gmail.com>
2024-04-20 20:14:12 +0300

    serve the web app from the rust server

--- pong-web/src/routes/+page.ts
+++ pong-web/src/routes/+page.ts
@@ -1,6 +1,7 @@
 import type { PageLoad } from './$types';
 
 export const ssr = false;
+export const prerender = true
 
 export const load = (async () => {
     console.log('page loading')
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,33 @@ mod network;
 mod new_matchmaking;
 mod runner;
 mod utils;
+use rust_embed::RustEmbed;
+use warp::Filter;
 
-fn main() {
-    network::websocket::start();
+#[derive(RustEmbed)]
+#[folder = "web/dist"]
+struct Static;
+
+#[tokio::main]
+async fn main() {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("myapp" / "hello" / String).map(|name| format!("Hello, {}!", name));
+
+    let static_files = warp::path("myapp").and(warp::get()).and(warp_embed::embed(&Static)).boxed();
+
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["GET", "POST"])
+        .allow_headers(vec!["Content-Type"]);
+
+    log::info!("Serving myapp on http://localhost:3030/myapp");
+    std::thread::spawn(|| {
+        network::websocket::start();
+    });
+    warp::serve(hello.or(static_files).with(cors)).run(([0, 0, 0, 0], 3030)).await;
 }
